9df60854bbb0e6adef71bc957b518b7e8feb19cd
Chris Mbah <mbahchris46@gmail.com>
2024-01-08 02:09:50 +0100

    Nav anbd custom nav theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,47 +2,64 @@
 import { Dropdown, Navbar, Button } from "flowbite-react";
 import { customButtonTheme } from "../themes/customButtton";
 import { customNavTheme } from "../themes/customNav";
-import {customDropdownTheme} from "../themes/customDropdown"
-
+import { customDropdownTheme } from "../themes/customDropdown";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <Navbar fluid rounded theme={customNavTheme} className="font-dmSans fixed w-full shadow-sm">
-      <Navbar.Brand href="/" className="md:w-[30%] w-[50%]">
-        <div className="flex ">
-          <img
-            src="../../public/logo.png"
-            className="mr-1 sm:mr-3 h-10 ss:h-14 sm:h-16"
-            alt="PTE Logo"
-          />
-          <div className=" w-[100%] sm:w-[70%] self-center whitespace-wrap text-[10px] xss:text-[13px] ss:text-base md:text-lg font-bold dark:text-white">
-            Polymer and Textile Engineering, FUTO
+    <nav className="">
+      <Navbar
+        fluid
+        rounded
+        theme={customNavTheme}
+        className="fixed w-full shadow-sm"
+      >
+        <Navbar.Brand href="/" className="md:w-[30%] w-[50%]">
+          <div className="flex ">
+            <img
+              src="../../public/logo.png"
+              className="mr-1 sm:mr-3 h-10 ss:h-14 sm:h-16"
+              alt="PTE Logo"
+            />
+            <div className=" w-[100%] sm:w-[70%] self-center whitespace-wrap text-[10px] xss:text-[13px] ss:text-base md:text-lg font-bold dark:text-white">
+              Polymer and Textile Engineering, FUTO
+            </div>
           </div>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Button
+            theme={customButtonTheme}
+            color="primary"
+            size="md"
+            className="focus:outline-none"
+          >
+            <Link to={"/login"}>Log In</Link>
+          </Button>
+          <Navbar.Toggle className="ml-2 focus:outline-green1" />
         </div>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button theme={customButtonTheme} color="primary" size="md" className="focus:outline-none" >Log In</Button>
-        <Navbar.Toggle className="ml-2 focus:outline-green1" />
-      </div>
-      <Navbar.Collapse className="xmd:block xmd:w-auto" >
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="/">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">
-          <Dropdown arrowIcon={true} inline label={"Alumini"} theme={customDropdownTheme}>
-            <Dropdown.Item>About Us</Dropdown.Item>
-            <Dropdown.Item>Our Mission</Dropdown.Item>
-            <Dropdown.Item>Our Vision</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item>Student Bodies</Dropdown.Item>
-          </Dropdown>
-        </Navbar.Link>
+        <Navbar.Collapse className="xmd:block xmd:w-auto">
+          <Navbar.Link href="#" active>
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="/">About</Navbar.Link>
+          <Navbar.Link href="#">
+            <Dropdown
+              arrowIcon={true}
+              inline
+              label={"Alumini"}
+              theme={customDropdownTheme}
+            >
+              <Dropdown.Item>About Us</Dropdown.Item>
+              <Dropdown.Item>Our Mission</Dropdown.Item>
+              <Dropdown.Item>Our Vision</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item>Student Bodies</Dropdown.Item>
+            </Dropdown>
+          </Navbar.Link>
 
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+          <Navbar.Link href="#">Contact</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </nav>
   );
 }
--- src/themes/customNav.ts
+++ src/themes/customNav.ts
@@ -4,7 +4,7 @@ export const customNavTheme: CustomFlowbiteTheme["navbar"] = {
   link: {
     base: "block py-2 pr-4 pl-3 md:p-0 ",
     active: {
-      on: "text-sm ss:text-base  border-b border-gray-100 bg-cyan-700 text-green1 dark:text-green1 md:bg-transparent md:text-green1",
+      on: "text-sm ss:text-base border-b md:border-none border-gray-100 bg-cyan-700 text-green1 dark:text-green1 md:bg-transparent md:text-green1",
       off: "text-sm ss:text-base border-b border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-green1 md:dark:hover:bg-transparent md:dark:hover:text-white",
     },
   },
