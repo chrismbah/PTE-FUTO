1052f44b22f45c3e8e097d6f008d11d009292ceb
Chris Mbah <mbahchris46@gmail.com>
2024-01-09 23:38:45 +0100

    Added user context

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,12 @@
+// import { createContext, useContext } from "react";
+// import { UserProviderProps } from "../models/context";
+
+// export const UserContext = createContext(null);
+
+// export function useUserContext() {
+//   return useContext(UserContext);
+// }
+
+// export default function UserContextProvider({ children }: UserProviderProps) {
+//   return (<UserContext.Provider value={}>{children}</UserContext.Provider>;)
+// }
---
+++ src/models/context.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export interface UserProviderProps{
+    children:ReactNode
+}
