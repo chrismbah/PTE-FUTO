be3f5b1d2b0c1fc3c90218b93b42cb4c93a1abdc
Chris Mbah <mbahchris46@gmail.com>
2024-01-10 00:18:03 +0100

    Minor changes...trying to fix build error

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
