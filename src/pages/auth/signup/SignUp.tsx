ff2f4c5f03259fcaece80402674b4b1669d6d41b
Chris Mbah <mbahchris46@gmail.com>
2024-01-10 00:21:39 +0100

    Checking..

--- src/pages/auth/signup/SignUp.tsx
+++ src/pages/auth/signup/SignUp.tsx
@@ -150,7 +150,7 @@ export default function SignUp() {
           </div>
           <button
             type="submit"
-            className="mb-4 text-white bg-green1 hover:bg-green2 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green1 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="mb-4 text-white bg-green1 hover:bg-green2  font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
             Sign Up
           </button>
