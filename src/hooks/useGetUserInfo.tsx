f3b8924aec461f535aba4379137ea7ce295b73ef
Chris Mbah <mbahchris46@gmail.com>
2024-01-13 00:58:34 +0100

    Fixed bug in hook

---
+++ src/hooks/useGetUserInfo.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../config/firebase";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  //   deleteDoc,
+  //   doc,
+} from "firebase/firestore";
+import { StudentDetails } from "../models/studentDetails";
+
+export const useGetUserInfo = () => {
+  const [studentDetails, setStudentDetails] = useState<StudentDetails | null>(null);
+  const [user] = useAuthState(auth);
+  const userID = user?.uid;
+
+  const userInfoRef = collection(db, "userInfo");
+
+  const userInfoDoc = userID
+    ? query(userInfoRef, where("userID", "==", userID))
+    : null;
+
+  const getUserInfo = async () => {
+    if (userInfoDoc) {
+      const userInfo = await getDocs(userInfoDoc);
+      const userFields = userInfo.docs[0].data();
+      const { firstName, lastName, regNo, email, level } = userFields;
+      setStudentDetails({
+        firstName: firstName,
+        lastName: lastName,
+        regNo: regNo,
+        email: email,
+        level: level,
+      });
+    } else {
+      console.log("User ID not yet available.");
+    }
+  };
+
+  return { getUserInfo, studentDetails };
+};
