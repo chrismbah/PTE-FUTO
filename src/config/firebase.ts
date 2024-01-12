621ad89d3cf2e770789595f7f0cd805a90255747
Chris Mbah <mbahchris46@gmail.com>
2024-01-12 01:01:27 +0100

    Implemented firestore database

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -15,3 +15,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig); 
 export const auth = getAuth(app)
+export const db = getFirestore(app)
