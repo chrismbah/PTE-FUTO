c742074822bb48f6a18c9a6460d093123c52e5f5
Chris Mbah <mbahchris46@gmail.com>
2024-01-13 01:00:24 +0100

    Testing fixed hook

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../config/firebase";
 export default function Home() {
-  return (
-    <>
-    </>
-  );
+  const [user] = useAuthState(auth)
+  // const userID = user?.uid
+  
+  const { getUserInfo, studentDetails } = useGetUserInfo(); 
+  // if (user){
+    useEffect(()=>{
+      getUserInfo();
+    },[user])
+  // }
+  return <></>;
 }
