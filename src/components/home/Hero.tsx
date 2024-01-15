3a1989d763b759893a986a9f47221e5ca00dc022
Chris Mbah <mbahchris46@gmail.com>
2024-01-15 23:19:25 +0100

    Refactoring design

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,23 +1,26 @@
 import hero from "../../assets/svg/home/hero.svg";
-
+import { Button } from "flowbite-react";
+import { customButtonTheme } from "../../themes/customButtton";
+import { Link } from "react-router-dom";
 export default function Hero() {
   return (
-    <div className="hero section">
-      <div className="section-flex-between flex-wrap">
+    <div className="hero section 2xl:max-w-[1280px] w-full mx-auto">
+      <div className="flex-between flex-col md:flex-row">
         <div>
-          <p className="text-xl">Welcome to</p>
-          <h1>Polymer and Textile Engineering, FUTO</h1>
-          <h2>Federal University of Technology, Owerri</h2>
-          <p>
+          {/* <p className="text-xl">Welcome to</p> */}
+          <h1 className="text-green1 xl:text-5xl md:text-4xl font-semibold">Polymer and Textile Engineering, FUTO</h1>
+          <h2 className="mb-3 xl:text-4xl md:text-3xl font-[500]">Federal University of Technology, Owerri</h2>
+          <p className="xl:text-lg text-base mb-8">
             Here, you can access course outlines, learning materials, calculate{" "}
             <br />
             your CGPA, stay updated with the latest news, and connect with a{" "}
             <br />
             thriving community.
           </p>
+          <Button theme={customButtonTheme} color="primary" size={"lg"} ><Link to={"/"}>Get started</Link></Button>
         </div>
-        <div>
-          <img src={hero} alt="School Image" className="w-[500px] h-[500px]"/>
+        <div className="col">
+          <img src={hero} alt="School Image" className="w-[800px]"/>
         </div>
       </div>
     </div>
