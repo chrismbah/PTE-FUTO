8d057e795b43ff2c1c97fd1c105550d5e77e3bd7
Chris Mbah <mbahchris46@gmail.com>
2024-01-16 20:55:44 +0100

    Added CalculateGP component

---
+++ src/components/home/CalculateGP.tsx
@@ -0,0 +1,32 @@
+import calculateGP from "../../assets/svg/home/calculateGP.svg";
+import { Button } from "flowbite-react";
+import { customButtonTheme } from "../../themes/customButtton";
+import { Link } from "react-router-dom";
+export default function CalculateGP() {
+  return (
+    <div className="box-width">
+      <div className="section section-flex-between">
+        <div className="w-full">
+          <h2 className="mb-5">Calculate Your GPA</h2>
+          <p className="mb-5">
+            Here you can track your academic progress and plan your journey at{" "}
+            <br />
+            Polymer and Textile Engineering FUTO. Your CGPA is key, reflecting
+            your academic <br />
+            performance and commitment to excellence.
+          </p>
+          <Button theme={customButtonTheme} size={"lg"} color="primary">
+           <Link to="/calculate-gp">Calculate Here</Link>
+          </Button>
+        </div>
+        <div>
+          <img
+            src={calculateGP}
+            alt="Calculate Your GP"
+            className="w-[500px] xmd:w-[800px] xmd:min-w-[300px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 import Hero from "../../components/home/Hero";
 import About from "../../components/home/About";
+import CalculateGP from "../../components/home/CalculateGP";
 export default function Home() {
   return (
     <>
      <Hero />
      <About />
+     <CalculateGP />
     </>
   );
 }
