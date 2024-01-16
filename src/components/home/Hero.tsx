f8d04cebca62a13d33ee82a72e744182620cabe2
Chris Mbah <mbahchris46@gmail.com>
2024-01-16 14:41:24 +0100

    Minor changes

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,14 +4,14 @@ import { customButtonTheme } from "../../themes/customButtton";
 import { Link } from "react-router-dom";
 export default function Hero() {
   return (
-    <div className="hero section 2xl:max-w-[1380px] w-full mx-auto">
-      <div className="flex-between flex-col md:flex-row">
+    <div className="hero-section box-width">
+      <div className="section-flex-between">
         <div className="w-full">
           {/* <p className="text-xl">Welcome to</p> */}
-          <h1 className="text-green1 xl:text-5xl lg:text-4xl md:text-3xl text-2xl font-semibold">
+          <h1 className="text-green1 xl:text-5xl lg:text-4xl text-3xl  font-semibold">
             Polymer and Textile Engineering, FUTO
           </h1>
-          <h2 className="mb-3 xl:text-4xl lg:text-3xl md:text-2xl font-[500]">
+          <h2 className="mb-3 xl:text-4xl lg:text-3xl md:text-2xl text-xl font-[500]">
             Federal University of Technology, Owerri
           </h2>
           <p className="xl:text-lg text-base md:mb-7 mb-4">
@@ -26,7 +26,7 @@ export default function Hero() {
           </Button>
         </div>
         <div className="col">
-          <img src={hero} alt="School Image" className="w-[800px]" />
+          <img src={hero} alt="School Image" className="w-[500px] md:w-[800px]" />
         </div>
       </div>
     </div>
