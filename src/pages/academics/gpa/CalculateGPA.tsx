bb4f3cf23d7e1ca331f7b483c9626a2ab10a34c6
Chris Mbah <mbahchris46@gmail.com>
2024-01-18 15:40:27 +0100

    Minor changes

--- src/pages/academics/gpa/CalculateGPA.tsx
+++ src/pages/academics/gpa/CalculateGPA.tsx
@@ -3,7 +3,7 @@ import GPAForm from "./components/GPAForm";
 export default function CalculateGPA() {
   return (
     <div className="bg-[url('../../../assets/svg/background/page-turner.svg')] min-h-screen w-full">
-      <div className="w-full min-h-screen flex items-center justify-center px-3 sm:px-10 py-10 pt-20">
+      <div className="bg-gray-50 w-full min-h-screen flex items-center justify-center px-3 sm:px-10 py-10 pt-20">
         <div className="bg-white p-5 sm:p-14 shadow-sm">
           <h2>GPA Calculator</h2>
           <p className="mb-4">
