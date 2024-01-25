8deb097d8ef98e18cce236e90460d3d495ebf3af
Chris Mbah <mbahchris46@gmail.com>
2024-01-25 10:54:54 +0100

    Checking if this works

--- src/pages/academics/course-outlines/CourseOutlineCard.tsx
+++ src/pages/academics/course-outlines/CourseOutlineCard.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import { CourseOutlines } from "../../../models/courseOutline";
 import { Link } from "react-router-dom";
-// import hsh from "../../../assets/icons/course-outline/levels/book1.png"
 export const CourseOutlineCard: FC<CourseOutlines> = ({
   icon,
   title,
@@ -10,26 +9,23 @@ export const CourseOutlineCard: FC<CourseOutlines> = ({
 }) => {
   return (
     <Link to={route}>
-        <div className="max-w-auto xsm:max-w-[460px] p-6 bg-white border border-gray-200 rounded-lg shadow-lg">
-      <img src={icon} alt="Book" className="w-16 h-16" />
-      <a href="#">
-        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          {title}
-        </h5>
-      </a>
-      <p className="mb-3 font-[500] text-gray-500 text-xs">
-        We have compiled a collection of course outlines tailored for {level} Level
-        students ensuring they have necessary information needed to make
-        informed decisions about your academic journey.
-      </p>
-      <a
-        href={route}
-        className="font-[600] inline-flex items-center text-green2 hover:underline"
-      >
-        Check Course Outline 
-      </a>
-    </div>
+      <div className="max-w-auto xsm:max-w-[460px] p-6 bg-white border border-gray-200 rounded-lg shadow-lg">
+        <img src={icon} alt="Book" className="w-16 h-16" />
+          <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+        <p className="mb-3 font-[500] text-gray-500 text-xs">
+          We have compiled a collection of course outlines tailored for {level}{" "}
+          Level students ensuring they have necessary information needed to make
+          informed decisions about your academic journey.
+        </p>
+        <a
+          href={route}
+          className="font-[600] inline-flex items-center text-green2 hover:underline"
+        >
+          Check Course Outline
+        </a>
+      </div>
     </Link>
-
   );
 };
--- src/utils/academics/course-outlines/courseOutlineLevels.ts
+++ src/utils/academics/course-outlines/courseOutlineLevels.ts
@@ -1,31 +1,31 @@
 export const courseOutlineLevels = [
   {
     level: "100",
-    icon: "../../../assets/icons/course-outline/levels/book1.png",
+    icon: "icons/course-outline/levels/book1.png",
     title: "100 Level",
     route: "/course-outlines/100-level",
   },
   {
     level: "200",
-    icon: "../../../../public/icons/course-outline/levels/book2.png",
+    icon: "icons/course-outline/levels/book2.png",
     title: "200 Level",
     route: "/course-outlines/200-level",
   },
   {
     level: "300",
-    icon: "../../../assets/icons/course-outline/levels/book3.png",
+    icon: "icons/course-outline/levels/book3.png",
     title: "300 Level",
     route: "/course-outlines/300-level",
   },
   {
     level: "400",
-    icon: "../../../../public/icons/course-outline/levels/book4.png",
+    icon: "icons/course-outline/levels/book4.png",
     title: "400 Level",
     route: "/course-outlines/400-level",
   },
   {
     level: "500",
-    icon: "../../../../public/icons/course-outline/levels/book5.png",
+    icon: "icons/course-outline/levels/book5.png",
     title: "500 Level",
     route: "/course-outlines/500-level",
   },
