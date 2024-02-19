d348e38412d0cbb10339ec0c8f9b7c0c9c7656a5
Chris Mbah <mbahchris46@gmail.com>
2024-02-19 23:00:12 +0100

    Fixed minor style error

--- src/pages/misc/blog/post/comments/Comments.tsx
+++ src/pages/misc/blog/post/comments/Comments.tsx
@@ -7,14 +7,14 @@ export const Comments: FC<PostCommentsProp> = ({ postComments }) => {
     <div>
       {postComments.map(({ firstName, lastName, comment, time, date }, i) => {
         return (
-          <div key={i} className="flex items-start pb-1 w-full gap-1 min-h-20">
+          <div key={i} className="flex items-start pb-1 w-full gap-1">
             <img src={userProfile} alt="profile" className="w-10 sm:w-12" />
             <div className="flex flex-col border-b border-gray-100 pb-2 w-[90%]">
               <div className="flex gap-3 items-center justify-start">
                 <div className="">
                   <p className="text-[11px] sm:text-[12px] text-gray-900">
                     {firstName} {lastName}
-                  </p>
+                  </p>  
                 </div>
                 <span className="text-sss sm:text-ss md:text-sss lg:text-ss text-gray-500">• {time}, {date}</span>
               </div>
