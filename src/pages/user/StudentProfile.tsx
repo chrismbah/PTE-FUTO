d6592d5e59d1d9fa3124a8b7e597d58cabc964b1
chrismbah <mbahchris46@gmail.com>
2024-02-29 16:53:51 +0100

    Update student profile

--- src/pages/user/StudentProfile.tsx
+++ src/pages/user/StudentProfile.tsx
@@ -13,29 +13,96 @@ import { ClockIcon } from "../../components/icons/ClockIcon";
 import { RegisterIcon } from "../../components/icons/RegisterIcon";
 import { notifyUser } from "../../helpers/notifyUser";
 import { UploadIcon } from "../../components/icons/UploadIcon";
-
+import { ref, uploadBytesResumable } from "firebase/storage";
+import { storage } from "../../config/firebase";
 export default function Profile() {
-  const { getUserInfo, studentDetails } = useGetUserInfo();
+  const { getUserInfo, studentDetails, userID } = useGetUserInfo();
   useEffect(() => {
     getUserInfo();
   }, []);
   const [imageFile, setImageFile] = useState<File | null>(null);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  // const [uploadError, setUploadError] = useState<Error | null>(null);
+  // const [imageURL, setImageURL] = useState<string>("");
+
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files && e.target.files[0];
     if (selectedFile && selectedFile.type.startsWith("image/")) {
       setImageFile(selectedFile);
+      setUploadProgress(0);
     } else {
       notifyUser("error", "Please choose a valid image file (PNG or JPG).");
       e.target.value = "";
     }
   };
 
+  const uploadProfileImage = async () => {
+    if (!imageFile) {
+      notifyUser("error", "Please select an image to upload.");
+      return;
+    }
+    try {
+      const storageRef = ref(
+        storage,
+        `profile-pictures/${studentDetails?.email}-${userID}/${imageFile.name}`
+      );
+      // Use the uploadBytesResumable function to upload the file to the storageRef
+      const uploadTask = uploadBytesResumable(storageRef, imageFile);
+      // listen to the state_changed event of the uploadTask
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // calculate the percentage of upload
+          const percentage = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          // set the progress state with the percentage
+          setUploadProgress(percentage);
+        },
+        // Error callback
+        (err) => {
+          // setUploadError(err);
+          console.log(err);
+        },
+        // Success callback
+        async () => {
+          // get the download URL from the uploadTask's snapshot reference and set the url state
+          // await getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          //   // setImageURL(downloadURL);
+          // });
+        }
+      );
+      console.log("Image Uploaded");
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      notifyUser("error", "Failed to upload image. Please try again.");
+    }
+  };
+
+  const renderProgressBar = () => {
+    if (imageFile) {
+      return (
+        <div className="progress-bar w-full ">
+          <div className="w-fit">
+            <p className="text-xs text-left font-semibold text-gray-700">
+              {uploadProgress} % Uploaded
+            </p>
+            <div className="h-1.5 w-full bg-gray-400 transition duration-300 ease-linear rounded-full">
+              <div className={`bg-green1 h-full block rounded-full`}></div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="box-width">
         <div className="page-section">
-          <div className="w-full flex items-center justify-between flex-col xss:flex-row">
-            <div className="flex flex-col xss:flex-row items-center gap-3">
+          <div className="w-full flex items-center justify-between flex-col md:flex-row gap-4">
+            <div className="flex flex-col xss:flex-row items-center gap-3 w-full">
               <Lottie
                 animationData={avatar}
                 loop={false}
@@ -59,13 +126,13 @@ export default function Profile() {
                 </div>
               </div>
             </div>
-            <div className="flex items-center justify-between flex-col">
-              <p className="w-full text-left block text-sm font-medium text-gray-900">
+            <div className="flex items-center justify-between flex-col w-full md:w-fit">
+              <p className="w-full text-left block text-ss sm:text-sm font-medium text-gray-900">
                 Update Profile Picture
               </p>
-              <div className="flex items-center gap-4">
-                <div className="flex items-center">
-                  <div>
+              <div className="flex items-center gap-1 ss:gap-4 w-full flex-col ss:flex-row justify-start">
+                <div className="flex items-center justify-start w-full ss:w-fit ">
+                  <div className="">
                     <input
                       type="file"
                       id="profile-input"
@@ -75,8 +142,8 @@ export default function Profile() {
                     />
                     <label
                       htmlFor="profile-input"
-                      className="block text-slate-500 py-2 px-4
-                          border-0 text-xs font-medium bg-black
+                      className="block w-[150px] text-slate-500 py-2 px-4
+                          border-0 text-sm font-medium bg-black
                         text-white hover:bg-black/70 cursor-pointer rounded-l-lg"
                     >
                       Choose Image
@@ -86,20 +153,26 @@ export default function Profile() {
                     {imageFile ? imageFile.name : "No file chosen"}
                   </label>
                 </div>
-                <button className="py-2 px-4 rounded-lg bg-green1 text-sm font-semibold text-white">
-                  Upload
-                  <UploadIcon className="w-2 sm:w-4 inline ml-2" />
-                </button>
+                <div className="w-full">
+                  <button
+                    onClick={uploadProfileImage}
+                    className="flex items-center group py-2 px-2 ss:px-4 transition duration-300 rounded-lg bg-green1 text-xss ss:text-ss sm:text-sm font-semibold text-white"
+                  >
+                    <span>Upload</span>
+                    <UploadIcon className="w-2 sm:w-4 group-hover:animate-bounce inline ml-2 mb-1" />
+                  </button>
+                </div>
               </div>
               <p
-                className="mt-1 w-full text-left text-ss text-gray-500 "
+                className="my-1 w-full text-left text-ss text-gray-500 "
                 id="file_input_help"
               >
-                PNG, JPG, WEBP Image.
+                PNG, JPG or WEBP Image.
               </p>
+              {renderProgressBar()}
             </div>
           </div>
-          <div className="ss:pl-4 sm:pl-[15px] md:pl-4 mt-5">
+          <div className="md:pl-4 mt-5">
             <h1 className="text-sm ss:text-xs xsm:text-base md:text-lg font-bold mb-2 sm:mb-3">
               Contact Information
             </h1>
@@ -169,3 +242,29 @@ export default function Profile() {
     </div>
   );
 }
+// Creating a reference to the upload location in Firebase Storage
+// const storageRef = ref(
+//   storage,
+//   `profile-pictures/${userID}/${imageFile.name}`
+// );
+// // Add `onUploadProgress` callback to update progress bar
+// const uploadTask = uploadBytes(storageRef, imageFile, {
+//   onUploadProgress: (snapshot) => {
+//     const progress = Math.round(
+//       (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+//     );
+//     setUploadProgress(progress);
+//   },
+// });
+// // Uploading the selected image using `put`
+// await uploadTask;
+// // Getting the download URL for the uploaded image
+// const downloadURL = await getDownloadURL(storageRef);
+
+// // Update user profile with the download URL (logic specific to your authentication provider)
+// // ... (This code will depend on your authentication system)
+
+// notifyUser("success", "Profile picture uploaded successfully!");
+
+// // Optionally, reset image selection state
+// setImageFile(null);
