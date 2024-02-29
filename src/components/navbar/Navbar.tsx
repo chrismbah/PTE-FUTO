5257b6bd860327855f9aaec349ab638c2b37a63e
chrismbah <mbahchris46@gmail.com>
2024-02-29 22:57:10 +0100

    Minor changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { useEffect } from "react";
@@ -14,9 +15,11 @@ import { useModalContext } from "../../context/Modal";
 import { SignOutModal } from "../modal/SignOutModal";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import { useUploadProfileImage } from "../../hooks/user-profile/useUploadProfileImage";
 
 export default function Nav() {
   const { getUserInfo, studentDetails, user, loading } = useGetUserInfo();
+  const { imageURL } = useUploadProfileImage()
   const firstName = studentDetails?.firstName;
   const lastName = studentDetails?.lastName;
   const email = studentDetails?.email;
@@ -24,7 +27,7 @@ export default function Nav() {
   const { setOpenSignOutModal } = useModalContext();
   useEffect(() => {
     getUserInfo();
-  }, [user]);
+  }, [user, studentDetails]);
 
   return (
     <nav className="">
@@ -47,7 +50,102 @@ export default function Nav() {
           </div>
         </Navbar.Brand>
         <div className="flex md:order-2">
-          {user ? (
+          {loading ? (
+            <Skeleton
+              circle={true}
+              className="h-[34px] w-[34px] md:h-[36px] md:w-[36px]"
+            />
+          ) : user ? (
+            studentDetails && studentDetails.profileImageURL.length > 1 ? (
+              <Dropdown
+                arrowIcon={false}
+                inline
+                className="z-[9999999] "
+                label={
+                  <Avatar
+                    alt="User"
+                    img={studentDetails.profileImageURL}
+                    rounded
+                    theme={customAvatar}
+                    size={"md"}
+                  />
+                }
+              >
+                <Dropdown.Header>
+                  <span className="block text-sm font-[600]">
+                    {studentDetails && firstName} {studentDetails && lastName}
+                  </span>
+                  <span className="block truncate text-sm">
+                    {studentDetails && email}
+                  </span>
+                </Dropdown.Header>
+                <Dropdown.Item>
+                  <Link to={"/dashboard"} className="w-full text-left">
+                    Dashboard
+                  </Link>{" "}
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  {" "}
+                  <Link to={"/profile"} className="w-full text-left">
+                    Profile
+                  </Link>{" "}
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={() => setOpenSignOutModal(true)}>
+                  Sign Out
+                </Dropdown.Item>
+              </Dropdown>
+            ) : (
+              <Dropdown
+                arrowIcon={false}
+                inline
+                className="z-[9999999] "
+                label={
+                  <Avatar
+                    alt="User"
+                    img={userProfileIcon}
+                    rounded
+                    theme={customAvatar}
+                    size={"md"}
+                  />
+                }
+              >
+                <Dropdown.Header>
+                  <span className="block text-sm font-[600]">
+                    {studentDetails && firstName} {studentDetails && lastName}
+                  </span>
+                  <span className="block truncate text-sm">
+                    {studentDetails && email}
+                  </span>
+                </Dropdown.Header>
+                <Dropdown.Item>
+                  <Link to={"/dashboard"} className="w-full text-left">
+                    Dashboard
+                  </Link>{" "}
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  {" "}
+                  <Link to={"/profile"} className="w-full text-left">
+                    Profile
+                  </Link>{" "}
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={() => setOpenSignOutModal(true)}>
+                  Sign Out
+                </Dropdown.Item>
+              </Dropdown>
+            )
+          ) : (
+            <Button
+              theme={customButtonTheme}
+              color="primary"
+              size="md"
+              className="focus:outline-none"
+            >
+              <Link to={"/login"}>Login</Link>
+            </Button>
+          )}
+          {/* {user ? (
             <Dropdown
               arrowIcon={false}
               inline
@@ -96,7 +194,7 @@ export default function Nav() {
             >
               <Link to={"/login"}>Login</Link>
             </Button>
-          )}
+          )} */}
           <Navbar.Toggle className="ml-2 focus:outline-green1 w-9 h-9" />
         </div>
         <Navbar.Collapse className="xmd:block xmd:w-auto z-[99999999]">
