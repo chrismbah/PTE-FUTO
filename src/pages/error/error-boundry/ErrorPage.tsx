3cd11d2a7fede9797eede6cdde935eef997d6117
chrismbah <mbahchris46@gmail.com>
2024-03-21 14:37:21 +0100

    Removed image in error page

--- src/pages/error/error-boundry/ErrorPage.tsx
+++ src/pages/error/error-boundry/ErrorPage.tsx
@@ -1,15 +1,15 @@
-import error from "../../../assets/svg/illustrations/error.svg";
+// import error from "../../../assets/svg/illustrations/error.svg";
 export default function ErrorPage() {
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col">
-      <img src={error} alt="Error" className="w-[80%] ss:w-[350px]" />
-      <p className="text-md ss:text-xl sm:text-2xl font-bold text-gray-900 text-center">
+      {/* <img src={error} alt="Error" className="w-[80%] ss:w-[350px]" /> */}
+      <p className="text-md ss:text-xl sm:text-3xl font-bold text-gray-900 text-center">
         Yikes! Something went wrong
       </p>
-      <p className="text-ss ss:text-sm sm:text-xs text-gray-700 text-center">
+      <p className="text-ss ss:text-sm sm:text-base text-gray-700 text-center">
         Brace yourself till we get the error fixed
       </p>
-      <p className="text-ss ss:text-sm sm:text-xs text-gray-700 text-center">
+      <p className="text-ss ss:text-sm sm:text-base text-gray-700 text-center">
         You may also{" "}
         <button
           className="text-green1 font-medium"
